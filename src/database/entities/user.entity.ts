4e149d0190c9b1e2d7120b4312cfe64be6509074
ZhinkinMark <zhinkin_ma@groupbwt.com>
2022-05-26 22:25:40 +0300

    feat(db): abb example model and migration

---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+enum Role {
+  USER,
+  ADMIN,
+  SUPERADMIN
+}
+
+@Entity('users')
+export class UserEntity {
+  @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'UUID.' })
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
+
+  @ApiProperty({ example: 'user@host.com', description: 'Account email.' })
+  @Index("ux_users_email", { unique: true })
+  @Column({ length: 32, unique: true, nullable: false, update: false})
+  email: string;
+
+  @ApiProperty({ example: 'd1e8a70...8943d082', description: 'Hashed password.' })
+  @Column({ length: 255, nullable: false, select: false })
+  hashedPassword: string;
+
+
+  @ApiProperty({ example: Role.USER, default: Role.USER, description: "Permission role." })
+  @Column('enum', { enum: Role, default: Role.USER, nullable: false })
+  role: Role;
+}
