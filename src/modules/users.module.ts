b8041d7a72decf7bce090e6cc02349d7f463222c
Stanislav Polos <polos_s@groupbwt.com>
2022-06-09 16:45:39 +0300

    feat: update users.module

--- src/modules/users.module.ts
+++ src/modules/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from 'src/controllers/user.controller';
+import { UserService } from 'src/services/user.service';
 import { UserEntity } from './../database/entities/user.entity';
 
 @Module({
   imports: [
-   TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([UserEntity]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
-export class UsersModule {}
+export class UsersModule { }
