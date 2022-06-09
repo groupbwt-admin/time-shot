bfd66421d60fc082bf97d6d63955ad7debca7290
Stanislav Polos <polos_s@groupbwt.com>
2022-06-09 16:45:53 +0300

    feat: update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users.module';
+import { AuthModule } from './modules/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env'
-   }),
-   TypeOrmModule.forRoot(),
-   UsersModule
+    }),
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
