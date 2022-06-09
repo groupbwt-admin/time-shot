e4899d6249748e865e239db567baeb362bad291d
Stanislav Polos <polos_s@groupbwt.com>
2022-06-09 13:02:07 +0300

    feat: add user.resource

---
+++ src/common/admin-panel/resources/user.resource.ts
@@ -0,0 +1,54 @@
+import { ResourceWithOptions } from "admin-bro";
+import { UserEntity } from "src/database/entities/user.entity";
+import * as bcrypt from 'bcrypt';
+import canModifyUser from "../permissions/user.permission";
+
+
+const UserResource: ResourceWithOptions = {
+    resource: UserEntity,
+    options: {
+        properties: {
+            hashedPassword: {
+                isVisible: false,
+            },
+            password: {
+                type: 'string',
+                isVisible: {
+                    list: false, edit: true, filter: false, show: false,
+                },
+            },
+        },
+        actions: {
+            edit: {
+                isAccessible: canModifyUser,
+                before: async (request) => {
+                    if (request.payload.password) {
+                        request.payload = {
+                            ...request.payload,
+                            hashedPassword: await bcrypt.hash(
+                                request.payload.password, process.env.SECRET_KEY
+                            ),
+                        }
+                    }
+                    return request
+                },
+            },
+            new: {
+                isAccessible: canModifyUser,
+                before: async (request) => {
+                    if (request.payload.password) {
+                        request.payload = {
+                            ...request.payload,
+                            hashedPassword: await bcrypt.hash(
+                                request.payload.password, process.env.SECRET_KEY
+                            ),
+                        }
+                    }
+                    return request
+                },
+            }
+        }
+    },
+};
+
+export default UserResource;
