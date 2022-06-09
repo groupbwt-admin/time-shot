1e87e510daaf2c1925571ef610150810d682a30f
Stanislav Polos <polos_s@groupbwt.com>
2022-06-09 13:09:19 +0300

    feat: update user resource

--- src/common/admin-panel/resources/user.resource.ts
+++ src/common/admin-panel/resources/user.resource.ts
@@ -46,6 +46,9 @@ const UserResource: ResourceWithOptions = {
                     }
                     return request
                 },
+            },
+            delete: {
+                isAccessible: canModifyUser,
             }
         }
     },
