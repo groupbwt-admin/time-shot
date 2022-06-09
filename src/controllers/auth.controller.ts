a7a9842f1d91c31159d50c218bc8067c4b91a168
Stanislav Polos <polos_s@groupbwt.com>
2022-06-09 16:41:13 +0300

    feat: add auth.controller

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from 'src/services/auth.service';
+
+@Controller('api')
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ) { }
+
+    @Post('auth/login')
+    async login(@Body() body): Promise<Object> {
+        return await this.authService.login(body);
+    }
+}
