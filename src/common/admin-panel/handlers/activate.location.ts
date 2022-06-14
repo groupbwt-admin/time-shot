488b0711a80a139039de161ffd9c597544fc55f5
Stanislav Polos <polos_s@groupbwt.com>
2022-06-14 16:02:12 +0300

    feat: update activateLocation

--- src/common/admin-panel/handlers/activate.location.ts
+++ src/common/admin-panel/handlers/activate.location.ts
@@ -2,20 +2,18 @@ import { LocationEntity } from "../../../database/entities/location.entity";
 
 const activateLocatin = async (request, response, context) => {
   const location = context.record;
-  const Locations = context._admin.findResource('LocationEntity');
-  const useLocation = await LocationEntity.findOne(context.record.param('locationId'));
-  console.log(useLocation.isActive.readInt());
-  console.log(Boolean(useLocation.isActive.readInt8()));
+  const useLocation = await LocationEntity.findOne(location.params.id);
+
   if (Boolean(useLocation.isActive.readInt8())) {
     return {
       record: location.toJSON(context.currentAdmin),
     }
   };
+
   useLocation.isActive = 1;
   await LocationEntity.save(useLocation)
-  console.log(useLocation);
   location.param = useLocation;
-  console.log("Active");
+
   return {
     record: location.toJSON(context.currentAdmin),
   }
