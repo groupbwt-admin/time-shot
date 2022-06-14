fd624a830d7caddb929f1b122ac3139cf11c1af1
Stanislav Polos <polos_s@groupbwt.com>
2022-06-14 16:18:09 +0300

    feat: update Dashboard

--- src/common/admin-panel/components/activated-locations.tsx
+++ src/common/admin-panel/components/activated-locations.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { H1 } from 'react-router-dom';
 
 const Dashboard = (props) => {
+  const location = props.record.params;
   return (
-    <Route path="/admin"></Route>
+    <div>
+      <h1>ID: {location.id.toString()}</h1>
+      <h1>Creator Email: {location.creator_email.toString()}</h1>
+      <h1>Name: {location.name.toString()}</h1>
+      <h1>Is Active: {Boolean(location.isActive.data[0]).toString()}</h1>
+    </div>
   )
 }
 export default Dashboard
