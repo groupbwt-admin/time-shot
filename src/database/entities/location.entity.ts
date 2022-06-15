6afece23a802c4ec3430c9faa863dc7416f773df
Stanislav Polos <polos_s@groupbwt.com>
2022-06-15 11:41:39 +0300

    feat: add entity location

---
+++ src/database/entities/location.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Index,
+  UpdateDateColumn,
+  CreateDateColumn,
+  DeleteDateColumn,
+  BaseEntity,
+  ManyToOne,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { UserEntity } from './user.entity';
+
+@Entity('locations')
+export class LocationEntity extends BaseEntity {
+  @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'UUID.' })
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174121', description: 'UUID creator.' })
+  @Index("ux_locations_creator", { unique: true })
+  @ManyToOne(() => UserEntity)
+  creator: string;
+
+  @ApiProperty({ example: "First Location", description: "Name location." })
+  @Column({ length: 256, unique: true })
+  name: string;
+
+  @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: "timestamp", default: null })
+  deletedAt?: Date;
+}
