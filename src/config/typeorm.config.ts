870eaf3ab7c7002bea09b5221c3aef64f6a2d354
Gusev Anton <gusev_aa@groupbwt.com>
2022-06-15 14:20:56 +0300

    fix: fix typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,21 +1,20 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm/dist/interfaces/typeorm-options.interface";
-import { join } from "path";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: "mysql",
+    type: 'mysql',
     host: process.env.TYPEORM_HOST,
-    port: parseInt(process.env.TYPEORM_PORT),
-    username: process.env.TYPEORM_USERNAME,
+    port: parseInt(process.env.TYPEORM_PORT, 10),
+    username: 'root',
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
-    entities: [join(__dirname, '..', '**', '*.entity{.ts,.js}')],
-    migrations: [join(__dirname, '..', 'database', 'migrations', '*.{.ts,.js}')],
+    entities: [__dirname + '/../database/entities/*.entity.{js,ts}'],
+    migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+    cli: {
+        migrationsDir: './src/database/migrations'
+    },
     extra: {
         charset: 'utf8mb4_unicode_ci'
     },
-    cli: {
-        migrationsDir: join(__dirname, '..', 'database', 'migrations')
-    },
     logging: process.env.TYPEORM_LOGGING.toLowerCase() === 'true',
-    synchronize: process.env.TYPEORM_SYNCHRONIZE.toLowerCase() === 'true'
+    synchronize: process.env.TYPEORM_SYNCHRONIZE.toLowerCase() === 'true',
 };
