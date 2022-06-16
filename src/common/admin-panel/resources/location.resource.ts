fddfaf6a9565b71605bc58cf55961188ab953a99
Stanislav Polos <polos_s@groupbwt.com>
2022-06-16 12:07:55 +0300

    feat: update location resource

--- src/common/admin-panel/resources/location.resource.ts
+++ src/common/admin-panel/resources/location.resource.ts
@@ -1,9 +1,38 @@
-import { ResourceWithOptions } from "admin-bro";
+import AdminBro, { ResourceWithOptions } from "admin-bro";
 import { LocationEntity } from "../../../database/entities/location.entity";
+import activateLocation from "../handlers/activate.location";
+import canActivateLocation from "../permissions/location.activate.permission";
+import canGrantPermission from "../permissions/user.permission";
 
 const LocationResource: ResourceWithOptions = {
   resource: LocationEntity,
-  options: {},
+  options: {
+    listProperties: ['name', 'creatorId', 'isActive'],
+    navigation: {
+      icon: "Location",
+      name: null,
+    },
+    actions: {
+      activateLocation: {
+        isAccessible: canActivateLocation,
+        actionType: 'record',
+        handler: activateLocation,
+        component: AdminBro.bundle('../components/activated-locations'),
+      },
+      show: {
+        isAccessible: canGrantPermission,
+      },
+      new: {
+        isAccessible: canGrantPermission,
+      },
+      edit: {
+        isAccessible: canGrantPermission,
+      },
+      delete: {
+        isAccessible: false
+      },
+    }
+  },
 };
 
 export default LocationResource;
