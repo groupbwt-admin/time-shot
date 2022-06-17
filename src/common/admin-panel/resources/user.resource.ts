f8e38d084db13973064d1f065cef3f27aea20f52
ZhinkinMark <zhinkin_ma@groupbwt.com>
2022-06-17 13:46:05 +0300

    feat(*): expand resourses and components - expand user resourse - add custom user edit component

--- src/common/admin-panel/resources/user.resource.ts
+++ src/common/admin-panel/resources/user.resource.ts
@@ -1,54 +1,141 @@
-import { ResourceWithOptions } from "admin-bro";
+import { ResourceWithOptions, ValidationError } from "admin-bro";
 import { UserEntity } from "src/database/entities/user.entity";
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from "bcrypt";
 import canModifyUser from "../permissions/user.permission";
-
+import  { perPageLimit } from "../../constants/adminjs-constants";
+import AdminBro from "admin-bro";
+import { Role } from "../../enums/role.enum"
 
 const UserResource: ResourceWithOptions = {
     resource: UserEntity,
     options: {
+        navigation: {
+            name: "Time tracking"
+        },
         properties: {
+            id: {
+                type: "string",
+                isVisible: {
+                    list: false, edit: false, filter: false, show: false
+                }
+            },
+            email: {
+                type: "string",
+                isVisible: {
+                    list: true, edit: true, filter: true, show: true
+                },
+                isTitle: true
+            },
             hashedPassword: {
-                isVisible: false,
+                type: "string",
+                isVisible: {
+                    list: false, edit: false, filter: false, show: false,
+                }
             },
             password: {
-                type: 'string',
+                type: "password",
                 isVisible: {
                     list: false, edit: true, filter: false, show: false,
+                }
+            },
+            duplicatePassword: {
+                type: "password",
+                isVisible: {
+                    list: false, edit: true, filter: false, show: false,
+                }
+            },
+            role: {
+                isVisible: {
+                    list: true, edit: true, filter: true, show: false,
                 },
+                custom: {
+                    user: Role.USER,
+                    admin: Role.ADMIN,
+                    superadmin: Role.SUPERADMIN
+                }
+            },
+            deletedAt: {
+                type: "date",
+                isVisible: {
+                    list: false, edit: true, filter: true, show: true,
+                }
             },
         },
         actions: {
             edit: {
-                isAccessible: canModifyUser,
-                before: async (request) => {
+                before: async (request, context) => {
+                    const isAccessible = canModifyUser(context);
+                    request.payload.email = null;
                     if (request.payload.password) {
+                        if (request.payload.password !== request.payload.duplicatePassword) {
+                            const message = "Password and duplicate password must be the same.";
+                            throw new ValidationError({name: {message: message}}, {message: message});
+                        }
                         request.payload = {
                             ...request.payload,
                             hashedPassword: await bcrypt.hash(
                                 request.payload.password, process.env.SECRET_KEY
-                            ),
+                            )
                         }
                     }
-                    return request
+                    if (request.payload.role && context.currentAdmin.role !== Role.SUPERADMIN) {
+                        const message = `${context.currentAdmin.role.toUpperCase()} can't set roles at all.`;
+                        throw new ValidationError({name: {message: message}}, {message: message});
+                    } else if (request.payload.id === context.currentAdmin.id) {
+                        const message = `You can't change yourself role. 
+                        Please, contact another ${Role.SUPERADMIN}.`;
+                        throw new ValidationError({name: {message: message}}, {message: message});
+                    }
+                    if (request.payload.deletedAt) {
+                        request.payload.deletedAt = isAccessible ? new Date() : null;
+                    }
+                    return request;
                 },
+                component: AdminBro.bundle('../../components/user-edit')
+            }, 
+            list: {
+                before: async (request, context) => {
+                    if (!canModifyUser(context)) {
+                        request.query.filters = {};
+                        request.query.filters.id = context.currentAdmin.id;
+                    }
+                    request.query.perPage = perPageLimit;
+                    return request;
+                }
             },
             new: {
                 isAccessible: canModifyUser,
-                before: async (request) => {
-                    if (request.payload.password) {
-                        request.payload = {
-                            ...request.payload,
-                            hashedPassword: await bcrypt.hash(
-                                request.payload.password, process.env.SECRET_KEY
-                            ),
-                        }
+                before: async (request, context) => {
+                    request.payload.deletedAt = null;
+                    if (!request.payload.email ) {
+                        const message = "Email is required.";
+                        throw new ValidationError({name: {message: message}}, {message: message});
                     }
-                    return request
+                    if (!request.payload.password || !request.payload.duplicatePassword) {
+                        const message = "Password and duplicate are required.";
+                        throw new ValidationError({name: {message: message}}, {message: message});
+                    } 
+                    if (request.payload.password !== request.payload.duplicatePassword) {
+                        const message = "Password and duplicate password must be the same.";
+                        throw new ValidationError({name: {message: message}}, {message: message});
+                    }
+                    if (!request.payload.role) {
+                        request.payload.role = Role.USER;
+                    }
+                    request.payload = {
+                        ...request.payload,
+                        hashedPassword: await bcrypt.hash(
+                            request.payload.password, process.env.SECRET_KEY
+                        ),
+                    }
+                    return request;
                 },
             },
             delete: {
-                isAccessible: canModifyUser,
+                isAccessible: false
+            },
+            bulkDelete: {
+                isAccessible: false
             }
         }
     },
---
+++ src/common/components/user-edit.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router'
+import { DrawerContent, Box, DrawerFooter, Button, Icon } from '@admin-bro/design-system'
+
+import PropertyType from 'admin-bro/src/frontend/components/property-type'
+import ActionHeader from 'admin-bro/src/frontend/components/app/action-header/action-header'
+import useRecord from 'admin-bro/src/frontend/hooks/use-record/use-record'
+import { appendForceRefresh } from 'admin-bro/src/frontend/components/actions/utils/append-force-refresh'
+import { useTranslation } from 'admin-bro/src/frontend/hooks/use-translation'
+import LayoutElementRenderer from 'admin-bro/src/frontend/components/actions/utils/layout-element-renderer'
+
+const CustomEdit = (props) => {
+  const { record: initialRecord, resource, action } = props;
+  resource.editProperties = resource.editProperties.filter(prop => prop.name !== "email");
+
+  const {
+    record,
+    handleChange,
+    submit: handleSubmit,
+    loading,
+    setRecord,
+  } = useRecord(initialRecord, resource.id)
+  const { translateButton } = useTranslation()
+  const history = useHistory()
+
+  useEffect(() => {
+    if (initialRecord) {
+      setRecord(initialRecord)
+    }
+  }, [initialRecord])
+
+  const submit = (event) => {
+    event.preventDefault()
+    handleSubmit().then((response) => {
+      if (response.data.redirectUrl) {
+        history.push(appendForceRefresh(response.data.redirectUrl))
+      }
+    })
+    return false
+  }
+
+  return (
+    <Box
+      as="form"
+      onSubmit={submit}
+      flex
+      flexGrow={1}
+      flexDirection="column"
+    >
+      <DrawerContent>
+        {action?.showInDrawer ? <ActionHeader {...props} /> : null}
+        {action.layout ? action.layout.map((layoutElement, i) => (
+          <LayoutElementRenderer
+            key={i}
+            layoutElement={layoutElement}
+            {...props}
+            where="edit"
+            onChange={handleChange}
+            record={record}
+          />
+        )) : resource.editProperties.map(property => (
+          <PropertyType
+            key={property.propertyPath}
+            where="edit"
+            onChange={handleChange}
+            property={property}
+            resource={resource}
+            record={record}
+          />
+        ))}
+      </DrawerContent>
+      <DrawerFooter>
+        <Button variant="primary" size="lg" type="submit" data-testid="button-save">
+          {loading ? (<Icon icon="Fade" spin />) : null}
+          {translateButton('save', resource.id)}
+        </Button>
+      </DrawerFooter>
+    </Box>
+  )
+}
+  
+export default CustomEdit;
