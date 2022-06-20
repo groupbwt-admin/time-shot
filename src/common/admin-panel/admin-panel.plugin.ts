79798a116c5fac4a41e64980c884f2ba7b2c5419
Gusev Anton <gusev_aa@groupbwt.com>
2022-06-20 15:26:11 +0300

    fix: fix admin panel

--- src/common/admin-panel/admin-panel.plugin.ts
+++ src/common/admin-panel/admin-panel.plugin.ts
@@ -2,9 +2,8 @@ import { Database, Resource } from "@admin-bro/typeorm";
 import { INestApplication } from "@nestjs/common";
 import AdminBro from "admin-bro";
 import buildAdminRouter from "./admin.router";
-import LocationResource from "./resources/location.resource";
 import UserResource from "./resources/user.resource";
-import LocationResource from './resources/locations.resource';
+import LocationResource from "./resources/location.resource";
 
 export async function setupAdminPanel(app: INestApplication): Promise<void> {
     AdminBro.registerAdapter({ Database, Resource });
