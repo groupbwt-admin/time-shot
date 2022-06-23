57ee40dc41f18482d586446ecc2b33d54acf94c5
Gusev Anton <gusev_aa@groupbwt.com>
2022-06-23 15:05:46 +0300

    feat(resource): add getTodayMillisecond action to time-shot resource

--- src/common/admin-panel/resources/time-shot.resource.ts
+++ src/common/admin-panel/resources/time-shot.resource.ts
@@ -1,26 +1,47 @@
 import { ResourceWithOptions } from "adminjs";
 import { TimeShotEntity } from "../../../database/entities/time-shot.entity";
 import hasAdminPermission from "../permissions/has-admin.permission";
+import { IsNull, Not, Raw } from "typeorm";
 
 const TimeShotResource: ResourceWithOptions = {
     resource: TimeShotEntity,
     options: {
         actions: {
             getWorkingTimeShot: {
-                icon: 'Activate',
-                actionType: 'record',
                 handler: async (request, response, context) => {
-                    const user = await TimeShotEntity.find({
+                    const timeShotEntity: TimeShotEntity = await TimeShotEntity.findOne({
                         select: ["id", "start"],
                         where: { stop: null }
                     });
-                    const record: object | null = user ? user : null;
 
+                    const record: object | null = timeShotEntity ? timeShotEntity : null;
                     return {
                         record
                     };
                 },
                 isVisible: false
+            },
+            getTodayMillisecond: {
+                handler: async (request, response, context) => {
+                    const timeShotEntities: TimeShotEntity[] = await TimeShotEntity.find({
+                        select: ["id", "start"],
+                        where: [{
+                            stop: Not(IsNull()),
+                            start: Raw(`DATE(${(new Date()).toISOString().substring(0, 10)})`)
+                        }]
+                    });
+
+                    let totalMillisecond: number = timeShotEntities.map(tse => {
+                            (new Date(tse.stop)).getTime();
+                            return -(new Date(tse.start)).getTime();
+                        }
+                    ).reduce((partialSum, a) => partialSum + a, 0);
+
+                    const record = {};
+                    return {
+                        totalMillisecond: totalMillisecond
+                    };
+                }
             },
             edit: { isAccessible: hasAdminPermission },
             delete: { isAccessible: hasAdminPermission },
