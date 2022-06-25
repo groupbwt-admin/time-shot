e2484db30e02d6b110fdbbc5f5f334926ac6ad49
Gusev Anton <gusev_aa@groupbwt.com>
2022-06-25 14:59:29 +0300

    feat(database): add craete table loactions migration

--- src/database/migrations/1656158131085-create-table-locations.ts
+++ src/database/migrations/1656158131085-create-table-locations.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createTableLocations1656086393045 implements MigrationInterface {
-    name = 'createTableLocations1656086393045';
+export class createTableLocations1656158131085 implements MigrationInterface {
+    name = 'createTableLocations1656158131085';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`locations\`
@@ -12,7 +12,6 @@ export class createTableLocations1656086393045 implements MigrationInterface {
                                      \`createdAt\` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                      \`updatedAt\` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                      \`deletedAt\` timestamp NULL,
-                                     INDEX         \`ix_locations_creator\` (\`creatorId\`),
                                      UNIQUE INDEX \`IDX_227023051ab1fedef7a3b6c7e2\` (\`name\`),
                                      PRIMARY KEY (\`id\`)
                                  ) ENGINE=InnoDB`);
@@ -24,8 +23,6 @@ export class createTableLocations1656086393045 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`locations\` DROP FOREIGN KEY \`FK_9eabb6aa5ae8bda167b26b77121\``);
         await queryRunner.query(`DROP
         INDEX \`IDX_227023051ab1fedef7a3b6c7e2\` ON \`locations\``);
-        await queryRunner.query(`DROP
-        INDEX \`ix_locations_creator\` ON \`locations\``);
         await queryRunner.query(`DROP TABLE \`locations\``);
     }
 
