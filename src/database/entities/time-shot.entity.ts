ea0a7e332483ddc30f76d4454116e286bb1ee512
Gusev Anton <gusev_aa@groupbwt.com>
2022-06-25 15:27:37 +0300

    feat: add api property to time-shot entity

--- src/database/entities/time-shot.entity.ts
+++ src/database/entities/time-shot.entity.ts
@@ -9,21 +9,26 @@ export class TimeShotEntity extends BaseEntity {
     @PrimaryGeneratedColumn("uuid")
     id: string;
 
+    @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'UUID.' })
     @ManyToOne(() => UserEntity, user => user.timeShots, { nullable: false })
     @JoinColumn({ name: "userId" })
     user: UserEntity;
 
+    @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'UUID.' })
     @ManyToOne(() => LocationEntity, location => location.startTimeShots, { nullable: false })
     @JoinColumn({ name: "locationStartId" })
     locationStart: LocationEntity;
 
+    @ApiProperty({ example: '123e4567-e89b-12d3-a456-426614174000', description: 'UUID.' })
     @ManyToOne(() => LocationEntity, location => location.endTimeShots, { nullable: true })
     @JoinColumn({ name: "locationEndId" })
     locationEnd: LocationEntity;
 
+    @ApiProperty({ example: '2022-06-25 12:07:31', description: 'timestamp' })
     @Column({ precision: null, type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
     start: Date;
 
+    @ApiProperty({ example: '2022-06-25 12:07:31', description: 'timestamp' })
     @Column({ precision: null, type: "timestamp", nullable: true })
     stop?: Date;
 }
