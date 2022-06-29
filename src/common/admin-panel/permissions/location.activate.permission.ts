9d23547be93f929a66101ff434efb5bcf2a4e5c3
Stanislav Polos <polos_s@groupbwt.com>
2022-06-29 15:46:26 +0300

    feat: update permission for activation location

--- src/common/admin-panel/permissions/location.activate.permission.ts
+++ src/common/admin-panel/permissions/location.activate.permission.ts
@@ -1,10 +1,12 @@
 import { Role } from "../../enums/role.enum";
 
 const canActivateLocation = (props) => {
+  const { currentAdmin } = props;
   return (
-    props.currentAdmin && (
-      props.currentAdmin.role === Role.ADMIN ||
-      props.currentAdmin.role === Role.SUPERADMIN
+    currentAdmin &&
+    !currentAdmin.location && (
+      currentAdmin.role === Role.ADMIN ||
+      currentAdmin.role === Role.SUPERADMIN
     )
   )
 };
