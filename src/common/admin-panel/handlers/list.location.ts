b964311218f294d853107620882927117a3714b1
Stanislav Polos <polos_s@groupbwt.com>
2022-06-29 12:48:17 +0300

    feat: add handler list location

--- src/common/admin-panel/handlers/list.location.ts
+++ src/common/admin-panel/handlers/list.location.ts
@@ -1,14 +1,65 @@
-import { LocationEntity } from '../../../database/entities/location.entity';
+import * as flat from 'flat'
+import { ActionContext, ActionRequest, Filter, populator } from 'adminjs';
+import sortSetter from 'adminjs/lib/backend/services/sort-setter/sort-setter';
+import parseJwt from '../../utils/parse-jwt';
+import parseCookiesFromActionRequest from '../../utils/parse-cookies-from-action-request';
 
-const listLocation = async (context) => {
-  for (let record of context.records) {
-    const location = record.params;
-    location.creatorEmail = (await LocationEntity.findOne({
-      where: { id: location.id },
-      relations: ['creator'],
-    })).creator['email'];
+const PER_PAGE_LIMIT = 500
+
+const listLocation = async (request: ActionRequest, response: any, context: ActionContext) => {
+  const cookie = parseCookiesFromActionRequest(request);
+  const { query } = request
+  const { sortBy, direction, filters = {} } = flat.unflatten(query || {})
+  const { resource } = context
+  let { page, perPage } = flat.unflatten(query || {})
+
+  if (perPage) {
+    perPage = +perPage > PER_PAGE_LIMIT ? PER_PAGE_LIMIT : +perPage
+  } else {
+    perPage = context._admin.options.settings?.defaultPerPage ?? 10
   }
-  return context
+  page = Number(page) || 1
+
+  const listProperties = resource.decorate().getListProperties()
+  const firstProperty = listProperties.find(p => p.isSortable())
+  let sort
+  if (firstProperty) {
+    sort = sortSetter(
+      { sortBy, direction },
+      firstProperty.name(),
+      resource.decorate().options,
+    )
+  }
+
+  const filter = await new Filter(filters, resource).populate()
+
+  let records, total
+  if (!cookie.accessToken) {
+    records = await resource.find(filter, {
+      limit: perPage,
+      offset: (page - 1) * perPage,
+      sort,
+    })
+    total = await resource.count(filter)
+  } else {
+    const { locationId } = parseJwt(cookie.accessToken);
+    records = [await resource.findOne(locationId)];
+    total = 1;
+  }
+  const populatedRecords = await populator(records)
+  // eslint-disable-next-line no-param-reassign
+  context.records = populatedRecords
+
+  return {
+    meta: {
+      total,
+      perPage,
+      page,
+      direction: sort?.direction,
+      sortBy: sort?.sortBy,
+    },
+    records: populatedRecords.map(r => r.toJSON(context.currentAdmin)),
+  }
 };
 
 export default listLocation;
