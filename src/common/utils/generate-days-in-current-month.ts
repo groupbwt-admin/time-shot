eb72efb21d7a67644b401ee59e02c88b8e4f2d94
ZhinkinMark <zhinkin_ma@groupbwt.com>
2022-06-30 16:40:47 +0300

    fix(utils): fix days generator

--- src/common/utils/generate-days-in-current-month.ts
+++ src/common/utils/generate-days-in-current-month.ts
@@ -1,17 +1,15 @@
 export default function generateDaysInCurrentMonth(month: number | null = null): Array<any> {
     const now = new Date();
-    if (!month) {
+    if (month === null) {
         month = now.getMonth();
     }
     let result = [];
     let weekNumber = 0;
 
-    const firstDayCurrentMonth = new Date(now.getFullYear(), month, 1);
-    const lastDayCurrentMonth = new Date(now.getFullYear(), month + 1, 0);
+    const lastDayCurrentMonth = new Date(Date.UTC(now.getFullYear(), month + 1, 0));
 
-    const difference = lastDayCurrentMonth.getTime() - firstDayCurrentMonth.getTime();
-    const days = Math.ceil(difference / (1000 * 3600 * 24));
-    for (let day = 1; day < days + 2; day++) {
+    const days = lastDayCurrentMonth.getDate()
+    for (let day = 1; day <= days; day++) {
         const fullDate = new Date(Date.UTC(now.getFullYear(), month, day));
         const dayNumber = fullDate.getDay() !== 0 ? fullDate.getDay() : 7;
 
