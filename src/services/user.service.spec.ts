f12343c6549cc9c54b72e54dfc95bb94e4b321ef
Stanislav Polos <polos_s@groupbwt.com>
2022-07-05 12:08:23 +0300

    feat: update test for user service

--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -7,9 +7,24 @@ import { UserService } from "./user.service";
 describe('UserService', () => {
     let service: UserService;
 
+    const mockUsers = [{
+        id: '2116ce44-f5e2-11ec-9b36-b42e999e335c',
+        email: 'user@user.com',
+        hashedPassword: '$2b$10$dfasdsdqwdqwdasdasd/yeAsC92cOK37eQ63omTOol00Du7yWUFQy',
+        role: 'superadmin',
+    }];
     const mockUserRepository = {
         create: jest.fn().mockImplementation(dto => dto),
-        save: jest.fn().mockImplementation(user => Promise.resolve({ ...user }))
+        save: jest.fn().mockImplementation(user => Promise.resolve({ ...user })),
+        find: jest.fn().mockResolvedValue(mockUsers),
+        findOne: jest.fn().mockImplementation(condition => {
+            const { where: { email } } = condition;
+            for (let user of mockUsers) {
+                if (user.email === email) {
+                    return Promise.resolve({ ...user });
+                };
+            };
+        }),
     };
 
     beforeEach(async () => {
@@ -42,5 +57,31 @@ describe('UserService', () => {
             hashedPassword: expect.any(String),
             role: Role.USER
         });
+    });
+
+    it('should return all users', async () => {
+        expect(await service.find()).toEqual([
+            {
+                id: '2116ce44-f5e2-11ec-9b36-b42e999e335c',
+                email: 'user@user.com',
+                hashedPassword: '$2b$10$dfasdsdqwdqwdasdasd/yeAsC92cOK37eQ63omTOol00Du7yWUFQy',
+                role: 'superadmin',
+            }
+        ]);
+    });
+
+    it('should find and return the user by email', async () => {
+        const email = 'user@user.com';
+        expect(await service.findOneByEmail(email)).toEqual({
+            id: '2116ce44-f5e2-11ec-9b36-b42e999e335c',
+            email: 'user@user.com',
+            hashedPassword: '$2b$10$dfasdsdqwdqwdasdasd/yeAsC92cOK37eQ63omTOol00Du7yWUFQy',
+            role: 'superadmin',
+        });
+    });
+
+    it('should return undefined if no user was found by email', async () => {
+        const email = 'user1@user1.com';
+        expect(await service.findOneByEmail(email)).toEqual(undefined);
     });
 });
